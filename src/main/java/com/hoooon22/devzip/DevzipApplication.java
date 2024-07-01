d8d89a02240dccb91aeb430283046aad888c92cb
Hoooon22 <momo990305@gmail.com>
2024-07-01 19:22:26 +0900

    First Commit

---
+++ src/main/java/com/hoooon22/devzip/DevzipApplication.java
@@ -0,0 +1,13 @@
+package com.hoooon22.devzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevzipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevzipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoooon22/devzip/DevzipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoooon22.devzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevzipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
