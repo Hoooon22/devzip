5a71e4ba8e9663ad808533a84ac908c8d1671763
Hoooon22 <momo990305@gmail.com>
2024-07-20 22:15:38 +0900

    project update

--- src/main/frontend/src/data/projects.js
+++ src/main/frontend/src/data/projects.js
@@ -10,46 +10,46 @@ const projects = [
     },
     { 
         id: 2, 
-        name: 'Project 2', 
-        description: '타임 킬링용 게임', 
-        link: 'https://www.google.com', 
-        active: true,
-        startDate: '2024-07-01',
-        endDate: ''
-    },
-    { 
-        id: 3, 
-        name: 'Project 3', 
-        description: '스토리 있는 로그라이크', 
-        link: 'https://www.github.com', 
-        active: true,
-        startDate: '',
-        endDate: '2023-09-30'
-    },
-    { 
-        id: 4, 
         name: '누구든지 흔적을 남기는 공간', 
         description: '들어와서 흔적을 남기세요.', 
         link: '/Guestbook', 
         active: true,
+        startDate: '2024-07-10',
+        endDate: ''
+    },
+    { 
+        id: 3, 
+        name: '타임 킬링용 게임', 
+        description: '...', 
+        link: '', 
+        active: false,
+        startDate: '2024-07-01',
+        endDate: ''
+    },
+    { 
+        id: 4, 
+        name: '스토리 있는 로그라이크', 
+        description: '...', 
+        link: '', 
+        active: false,
         startDate: '',
         endDate: '2023-09-30'
     },
     { 
         id: 5, 
-        name: '플레이어의 피드백을 직접 소통받으면서 실시간 업데이트를 진행하는 게임', 
-        description: '??', 
+        name: '피드백 게임', 
+        description: '플레이어의 피드백을 직접 소통받으면서 실시간 업데이트를 진행하는 게임', 
         link: '/Guestbook', 
-        active: true,
+        active: false,
         startDate: '',
         endDate: '2023-09-30'
     },
     { 
         id: 6, 
         name: '치료 목적 게임', 
-        description: '??', 
+        description: '...', 
         link: '/Guestbook', 
-        active: true,
+        active: false,
         startDate: '',
         endDate: '2023-09-30'
     },
