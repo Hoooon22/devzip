47a4d474687ca1dc326dfa7de6f9371dda8de7a0
Hoooon22 <momo990305@gmail.com>
2024-07-20 19:47:24 +0900

    guestbook scss

--- src/main/frontend/src/components/guestbook/EntryForm.js
+++ src/main/frontend/src/components/guestbook/EntryForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import '../assets/css/EntryForm.scss';
+import '../../assets/css/EntryForm.scss';
 
 const EntryForm = ({ addEntry }) => {
     const [name, setName] = useState('');
--- src/main/frontend/src/components/guestbook/EntryList.js
+++ src/main/frontend/src/components/guestbook/EntryList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../assets/css/EntryList.scss';
+import '../../assets/css/EntryList.scss';
 
 const EntryList = ({ entries }) => {
     return (
