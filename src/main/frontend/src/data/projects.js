20f807231f5b2c9beae3eed652671dc207e49eb9
Hoooon22 <momo990305@gmail.com>
2024-08-05 19:13:34 +0900

    delete empty project

--- src/main/frontend/src/data/projects.js
+++ src/main/frontend/src/data/projects.js
@@ -17,42 +17,42 @@ const projects = [
         startDate: '2024-07-10',
         endDate: ''
     },
-    { 
-        id: 3, 
-        name: '타임 킬링용 게임', 
-        description: '...', 
-        link: '', 
-        active: false,
-        startDate: '2024-07-01',
-        endDate: ''
-    },
-    { 
-        id: 4, 
-        name: '스토리 있는 로그라이크', 
-        description: '...', 
-        link: '', 
-        active: false,
-        startDate: '',
-        endDate: '2023-09-30'
-    },
-    { 
-        id: 5, 
-        name: '피드백 게임', 
-        description: '플레이어의 피드백을 직접 소통받으면서 실시간 업데이트를 진행하는 게임', 
-        link: '/Guestbook', 
-        active: false,
-        startDate: '',
-        endDate: '2023-09-30'
-    },
-    { 
-        id: 6, 
-        name: '치료 목적 게임', 
-        description: '...', 
-        link: '/Guestbook', 
-        active: false,
-        startDate: '',
-        endDate: '2023-09-30'
-    },
+    // { 
+    //     id: 3, 
+    //     name: '타임 킬링용 게임', 
+    //     description: '...', 
+    //     link: '', 
+    //     active: false,
+    //     startDate: '2024-07-01',
+    //     endDate: ''
+    // },
+    // { 
+    //     id: 4, 
+    //     name: '스토리 있는 로그라이크', 
+    //     description: '...', 
+    //     link: '', 
+    //     active: false,
+    //     startDate: '',
+    //     endDate: '2023-09-30'
+    // },
+    // { 
+    //     id: 5, 
+    //     name: '피드백 게임', 
+    //     description: '플레이어의 피드백을 직접 소통받으면서 실시간 업데이트를 진행하는 게임', 
+    //     link: '/Guestbook', 
+    //     active: false,
+    //     startDate: '',
+    //     endDate: '2023-09-30'
+    // },
+    // { 
+    //     id: 6, 
+    //     name: '치료 목적 게임', 
+    //     description: '...', 
+    //     link: '/Guestbook', 
+    //     active: false,
+    //     startDate: '',
+    //     endDate: '2023-09-30'
+    // },
     // 추가 프로젝트들...
 ];
 
