e0ec92bf415f53e7a2cde0978e1c4be1be9c30c0
Hoooon22 <momo990305@gmail.com>
2024-08-12 21:14:41 +0900

    WebSocket 연결 중복 방지

--- src/main/frontend/src/components/game/ChatWindow.js
+++ src/main/frontend/src/components/game/ChatWindow.js
@@ -7,10 +7,11 @@ const ChatWindow = ({ onNewMessage }) => {
   const [ws, setWs] = useState(null);
   const [connected, setConnected] = useState(false);
   const messagesEndRef = useRef(null);
-  const isSending = useRef(false); // 메시지 전송 중인지 여부를 추적
+  const isSending = useRef(false);
 
-  // WebSocket 연결 설정 및 재연결 로직
   const initializeWebSocket = useCallback(() => {
+    if (ws) return; // 이미 WebSocket이 설정된 경우 초기화하지 않음
+
     const socket = new WebSocket('wss://devzip.site/game-chatting');
 
     socket.onopen = () => {
@@ -32,46 +33,37 @@ const ChatWindow = ({ onNewMessage }) => {
     socket.onclose = (event) => {
       console.log('WebSocket connection closed', event.reason);
       setConnected(false);
-      setTimeout(initializeWebSocket, 5000); // 5초 후 재연결 시도
+      setWs(null); // WebSocket 연결이 닫혔을 때 ws 상태를 null로 설정
     };
 
     setWs(socket);
-
-    return () => {
-      if (socket.readyState === WebSocket.OPEN) {
-        socket.close();
-      }
-    };
-  }, [onNewMessage]);
+  }, [ws, onNewMessage]);
 
   useEffect(() => {
-    // WebSocket 초기화
     initializeWebSocket();
 
-    // 컴포넌트 언마운트 시 연결 종료
     return () => {
-      if (ws && ws.readyState === WebSocket.OPEN) {
+      if (ws) {
         ws.close();
       }
     };
-  }, [initializeWebSocket]);
+  }, [initializeWebSocket, ws]);
 
   const sendMessage = () => {
-    if (isSending.current) return; // 이미 메시지를 보내는 중이면 return
+    if (isSending.current) return;
 
     if (ws && ws.readyState === WebSocket.OPEN && message.trim()) {
-      isSending.current = true; // 메시지 전송 시작
+      isSending.current = true;
       ws.send(message);
       setMessage('');
       setTimeout(() => {
-        isSending.current = false; // 전송 후에 상태 초기화
-      }, 100); // 적절한 딜레이 설정
+        isSending.current = false;
+      }, 100);
     } else {
       console.error('WebSocket is not open or message is empty');
     }
   };
 
-  // Scroll to bottom of the chat
   useEffect(() => {
     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
   }, [messages]);
