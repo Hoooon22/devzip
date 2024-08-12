422b3ed4cad0e0ceb0d0f36175962faac4a46a0b
Hoooon22 <momo990305@gmail.com>
2024-08-12 17:37:09 +0900

    chatwindow

--- src/main/java/com/hoooon22/devzip/Config/WebSocketConfig.java
+++ src/main/java/com/hoooon22/devzip/Config/WebSocketConfig.java
@@ -14,7 +14,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatHandler(), "/chatting");
+        registry.addHandler(chatHandler(), "/game-chatting");
     }
 
     // WebSocketHandler bean 정의
