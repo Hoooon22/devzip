aae89d8bc0a66b1edcc4734e4302201f1fcb735b
Hoooon22 <momo990305@gmail.com>
2024-08-12 19:16:28 +0900

    setproxy websocket

--- src/main/frontend/src/setProxy.js
+++ src/main/frontend/src/setProxy.js
@@ -10,4 +10,14 @@ module.exports = function(app) {
       changeOrigin: true,
     })
   );
+
+  // WebSocket 요청 프록시 설정
+  app.use(
+    '/game-chatting',
+    createProxyMiddleware({
+      target: 'ws://localhost:8080',  // WebSocket 서버 URL
+      ws: true,  // WebSocket 연결을 프록시하도록 설정
+      changeOrigin: true,
+    })
+  );
 };
