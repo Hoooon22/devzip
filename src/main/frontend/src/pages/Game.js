1b740906060c2c40b7397602387c63d3346d9956
Hoooon22 <momo990305@gmail.com>
2024-08-12 17:11:16 +0900

    game recode

--- src/main/frontend/src/pages/Game.js
+++ src/main/frontend/src/pages/Game.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Character from '../components/game/Character';
+import ChatWindow from '../components/game/ChatWindow';
 
 const Game = () => {
   return (
     <div>
       <h1>Game Page</h1>
       <Character />
+      <ChatWindow />
     </div>
   );
 };
