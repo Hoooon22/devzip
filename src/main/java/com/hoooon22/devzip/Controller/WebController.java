e7ca56313fc0442b94e3c1729f6d0f67d5fdf3df
Hoooon22 <momo990305@gmail.com>
2024-08-12 16:26:40 +0900

    SpringBoot WebController Game

--- src/main/java/com/hoooon22/devzip/Controller/WebController.java
+++ src/main/java/com/hoooon22/devzip/Controller/WebController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
-    @GetMapping(value = {"/", "/Guestbook", "/error"})
+    @GetMapping(value = {"/", "/Guestbook", "Game", "/error"})
     public String index() {
         return "forward:/index.html";
     }
