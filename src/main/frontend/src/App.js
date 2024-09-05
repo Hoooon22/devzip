3b4958613e04cba88d6f2ef939c73b1ca5d4d5d0
Hoooon22 <momo990305@gmail.com>
2024-09-05 17:35:45 +0900

    game delete

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Main from "./pages/Main";
 import Guestbook from "./pages/Guestbook";
-import ThiefVsCop from "./pages/ThiefVsCop";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
@@ -14,8 +13,6 @@ function App() {
         {/* 방명록 페이지 */}
         <Route path="/Guestbook" element={<Guestbook />} />
 
-        {/* 경찰과 도둑 페이지 */}
-        <Route path="/ThiefVsCop" element={<ThiefVsCop />} />
       </Routes>
     </BrowserRouter>
   );
