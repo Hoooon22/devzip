ec88d774f3695d4853fb78dee5f49d9163f0aedb
Hoooon22 <momo990305@gmail.com>
2024-09-05 20:17:44 +0900

    WebConfig

--- src/main/java/com/hoooon22/devzip/Config/CorsConfig.java
+++ src/main/java/com/hoooon22/devzip/Config/CorsConfig.java
@@ -5,14 +5,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 엔드포인트에 대해 CORS 설정 적용
                 .allowedOrigins("https://devzip.site")  // 허용할 출처
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
-                .allowedHeaders("*");  // 허용할 헤더
-                // .allowCredentials(true);  // 자격 증명(쿠키, 인증 헤더 등) 허용
+                .allowedHeaders("Authorization", "Content-Type")
+                .allowCredentials(true);  // 자격 증명(쿠키, 인증 헤더 등) 허용
     }
 }
