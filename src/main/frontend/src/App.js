8792ce7195e50e30277ae3d9b9d4bda633d10390
Hoooon22 <momo990305@gmail.com>
2024-09-07 17:08:41 +0900

    app js LolPatch

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Main from "./pages/Main";
 import Guestbook from "./pages/Guestbook";
 import Joke from "./pages/Joke";
-import lolPatch from "./pages/lolPatch";
+import LolPatch from "./pages/LolPatch";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
@@ -19,7 +19,7 @@ function App() {
         <Route path="/Joke" element={<Joke />} />
 
         {/* 롤 패치노트 페이지 */}
-        <Route path="/lolPatch" element={<lolPatch />} />
+        <Route path="/LolPatch" element={<LolPatch />} />
 
       </Routes>
     </BrowserRouter>
