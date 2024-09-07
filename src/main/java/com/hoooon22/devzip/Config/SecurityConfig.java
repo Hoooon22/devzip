e715930d513c40a4246922b5c4dbcc373ff4cf3d
Hoooon22 <momo990305@gmail.com>
2024-09-07 16:15:55 +0900

    securityconfig

--- src/main/java/com/hoooon22/devzip/Config/SecurityConfig.java
+++ src/main/java/com/hoooon22/devzip/Config/SecurityConfig.java
@@ -1,16 +1,21 @@
 package com.hoooon22.devzip.Config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
 
-@Bean
-public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-        .headers(headers -> headers
-            .addHeaderWriter(new StaticHeadersWriter("Permissions-Policy",
-                "geolocation=(), microphone=()"))  // 불필요한 기능 비활성화
-        );
-    return http.build();
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .headers(headers -> headers
+                .addHeaderWriter(new StaticHeadersWriter("Permissions-Policy",
+                    "geolocation=(), microphone=()"))  // 불필요한 기능 비활성화
+            );
+        return http.build();
+    }
 }
