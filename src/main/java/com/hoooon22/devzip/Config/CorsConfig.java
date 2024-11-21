077a10869a0be7a78eb986b4650dc95be29c6f8f
Hoooon22 <momo990305@gmail.com>
2024-11-21 17:03:01 +0900

    cors setting allowedorigin all

--- src/main/java/com/hoooon22/devzip/Config/CorsConfig.java
+++ src/main/java/com/hoooon22/devzip/Config/CorsConfig.java
@@ -9,10 +9,10 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // 모든 엔드포인트에 대해 CORS 설정 적용
+        registry.addMapping("/api/v1/**")  // 모든 엔드포인트에 대해 CORS 설정 적용
                 .allowedOrigins("https://devzip.site")  // 허용할 출처
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
                 .allowedHeaders("Authorization", "Content-Type")
-                .allowCredentials(true);  // 자격 증명(쿠키, 인증 헤더 등) 허용
+                .allowCredentials(false);  // 자격 증명(쿠키, 인증 헤더 등) 허용
     }
 }
