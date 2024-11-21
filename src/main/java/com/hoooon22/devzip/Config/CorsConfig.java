fdadeeae9a87b4608eb3c1e385d188f07f7bf46c
Hoooon22 <momo990305@gmail.com>
2024-11-21 20:00:23 +0900

    entryform credential

--- src/main/java/com/hoooon22/devzip/Config/CorsConfig.java
+++ src/main/java/com/hoooon22/devzip/Config/CorsConfig.java
@@ -10,9 +10,12 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1/**")  // 모든 엔드포인트에 대해 CORS 설정 적용
-                .allowedOrigins("https://devzip.site")  // 허용할 출처
-                .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
-                .allowedHeaders("*")
-                .allowCredentials(true);  // 자격 증명(쿠키, 인증 헤더 등) 허용
+                .allowedOrigins(
+                    "https://devzip.site",  // 허용할 출처
+                    "http://localhost:3000"  // 로컬 개발 환경에서의 출처 추가 (필요 시)
+                )  
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")  // 필요한 HTTP 메서드 추가
+                .allowedHeaders("*")  // 모든 헤더 허용
+                .allowCredentials(true);  // 자격 증명 허용
     }
 }
