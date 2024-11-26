d1b0f1c308c9f5e7bea662028fc185eed946f6c7
Hoooon22 <momo990305@gmail.com>
2024-11-26 22:18:43 +0900

    Create Dashboard view

---
+++ src/main/frontend/src/components/dashboard/main/View.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "../../../assets/css/View.module.scss";
+
+const View = () => {
+    
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    );
+};
+
+export default View;
--- src/main/frontend/src/pages/Dashboard.js
+++ src/main/frontend/src/pages/Dashboard.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "../assets/css/Dashboard.scss";
 import Sidebar from "../components/dashboard/main/Sidebar";
+import View from "../components/dashboard/main/View";
 
 const Dashboard = () => {
 
     return (
         <div className="dashboard-container">
             <Sidebar />
+            <View />
         </div>
     );
 };
