1e8ede70d58b7b8c25ece0a5088b16442ca4579a
Hoooon22 <momo990305@gmail.com>
2025-01-21 21:59:23 +0900

    ready for message to backend

--- src/main/pixel-roguelike/src/utils/DungeonGenerator.js
+++ src/main/pixel-roguelike/src/utils/DungeonGenerator.js
@@ -13,8 +13,5 @@ export const generateDungeon = (setMessages) => {
   placeEnemies(dungeon);
   placeBoss(dungeon);
 
-  // 맵 생성 시 메시지를 추가 ($$$$$$$$$ 메시지 생성 작업 시작 해야 함!!!!!!!!)
-  setMessages((prevMessages) => [...prevMessages, '새로운 던전을 생성했습니다.']);
-
   return dungeon;
 };
