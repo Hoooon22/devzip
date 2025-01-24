1c48c116d25e81931ec2c9a33e2efeddfcf8e043
Hoooon22 <momo990305@gmail.com>
2025-01-24 19:58:56 +0900

    delete pixel-dungeon

--- src/main/java/com/hoooon22/devzip/Controller/WebController.java
+++ src/main/java/com/hoooon22/devzip/Controller/WebController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
+    
+    // Frontend 프로젝트의 라우팅 처리
     @GetMapping(value = {"/", "/Guestbook", "/Joke", "/lolPatch", "/apiPage", "/dashboard", "/error"})
     public String index() {
         return "forward:/index.html";
