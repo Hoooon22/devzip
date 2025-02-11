9c9529d6c5b79a4478a961b603c825e15aea0350
Hoooon22 <momo990305@gmail.com>
2025-02-11 21:54:04 +0900

    Add TrendChat

--- src/main/frontend/src/pages/ApiPage.js
+++ src/main/frontend/src/pages/ApiPage.js
@@ -5,8 +5,13 @@ const ApiPage = () => {
     return (
         <div className="apiPage-container">
             <h1>API Zip</h1>
+
             <h2>Developer</h2>
             <a href="/api/developer/emotion">/api/developer/emotion</a>
+
+            <h2>TrendChat</h2>
+            <a href="/api/trend/timestamp">/api/trend/timestamp</a>
+            <a href="/api/trend/keywords">/api/trend/keywords</a>
         </div>
     );
 };
