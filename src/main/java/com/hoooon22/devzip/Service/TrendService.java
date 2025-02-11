c8c5df8d302bee25fda1c948e3ac2e7f545a8ae3
Hoooon22 <momo990305@gmail.com>
2025-02-11 21:05:43 +0900

    api /api/trendingkeywords

--- src/main/java/com/hoooon22/devzip/Service/TrendService.java
+++ src/main/java/com/hoooon22/devzip/Service/TrendService.java
@@ -1,22 +1,24 @@
 package com.hoooon22.devzip.Service;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
-import org.springframework.stereotype.Service;
+import org.springframework.core.io.ClassPathResource;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hoooon22.devzip.Model.TrendingKeywords;
 
-@Service
 public class TrendService {
 
     public TrendingKeywords getTrendingKeywords() throws IOException {
-        // Json file
-        String filePath = "src/main/resources/trending_keywords.json";
+        // ClassPathResource를 사용하여 JSON 파일 읽기
+        ClassPathResource resource = new ClassPathResource("trending_keywords.json");
 
-        // ObjectMapper로 Json 파일을 Java 객체로 변환
+        // InputStream을 얻어 JSON 파일 읽기
+        InputStream inputStream = resource.getInputStream();
+
+        // ObjectMapper로 JSON 파일을 Java 객체로 변환
         ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(new File(filePath), TrendingKeywords.class);
+        return objectMapper.readValue(inputStream, TrendingKeywords.class);
     }
 }
