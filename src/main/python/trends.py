08a00d82fd57221b8837379e4887e57abd4cd233
Hoooon22 <momo990305@gmail.com>
2025-02-11 19:51:41 +0900

    trends.py run every 2 hours

--- src/main/python/trends.py
+++ src/main/python/trends.py
@@ -1,6 +1,7 @@
 import json
 import os
 from pytrends.request import TrendReq
+from datetime import datetime
 
 try:
     # 현재 파일이 위치한 경로 얻기
@@ -13,15 +14,18 @@ try:
     trending_searches_df = pytrends.trending_searches(pn='south_korea')
     top_keywords = trending_searches_df[0].tolist()
 
-    # 트렌드 목록을 JSON 형식으로 저장
-    result = {"top_keywords": top_keywords}
+    # 갱신 날짜 및 트렌드 목록을 JSON 형식으로 저장
+    result = {
+        "updated_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),  # 갱신 날짜 추가
+        "top_keywords": top_keywords
+    }
 
     # 현재 파일이 있는 경로에 JSON 파일로 저장
     json_file_path = os.path.join(script_dir, 'trending_keywords.json')
     with open(json_file_path, 'w', encoding='utf-8') as json_file:
         json.dump(result, json_file, ensure_ascii=False, indent=2)
 
-    print(f"트렌드 키워드가 '{json_file_path}' 파일로 저장되었습니다.")
+    print(f"트렌드 키워드와 갱신 날짜가 '{json_file_path}' 파일로 저장되었습니다.")
 
 except Exception as e:
     print(f"오류 발생: {e}")
