38c1f020ee8874ee717ed0161c503c471b9d2e06
Hoooon22 <momo990305@gmail.com>
2025-03-13 02:58:51 +0900

    ColorHandshakeInterceptor에서 HttpServletRequest를 ServletServerHttpRequest로 변경하여 클라이언트 IP 가져오는 로직 개선 및 디버깅 정보 출력 추가

--- src/main/java/com/hoooon22/devzip/Config/ColorHandshakeInterceptor.java
+++ src/main/java/com/hoooon22/devzip/Config/ColorHandshakeInterceptor.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
@@ -13,11 +14,12 @@ public class ColorHandshakeInterceptor implements HandshakeInterceptor {
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, 
                                    WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
-        if (request instanceof HttpServletRequest) {
-            HttpServletRequest servletRequest = (HttpServletRequest) request;
-            String ip = servletRequest.getHeader("X-Forwarded-For");
+        if (request instanceof ServletServerHttpRequest) {
+            ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
+            HttpServletRequest httpServletRequest = servletRequest.getServletRequest();
+            String ip = httpServletRequest.getHeader("X-Forwarded-For");
             if (ip == null || ip.isEmpty()) {
-                ip = servletRequest.getRemoteAddr();
+                ip = httpServletRequest.getRemoteAddr();
             }
             attributes.put("clientIp", ip);
             // 무작위 색상 생성
@@ -26,6 +28,7 @@ public class ColorHandshakeInterceptor implements HandshakeInterceptor {
             int blue = (int)(Math.random() * 256);
             String clientColor = String.format("#%02x%02x%02x", red, green, blue);
             attributes.put("clientColor", clientColor);
+            System.out.println("ColorHandshakeInterceptor: clientIp=" + ip + ", clientColor=" + clientColor);
         }
         return true;
     }
