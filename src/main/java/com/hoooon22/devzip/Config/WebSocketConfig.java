76b80d8888a5293201611dc7bb7b6ca980861079
Hoooon22 <momo990305@gmail.com>
2025-03-13 00:21:45 +0900

    WebSocketConfig에서 주석 제거 및 코드 정리

--- src/main/java/com/hoooon22/devzip/Config/WebSocketConfig.java
+++ src/main/java/com/hoooon22/devzip/Config/WebSocketConfig.java
@@ -9,18 +9,14 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // 클라이언트가 연결할 엔드포인트 (SockJS 지원)
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // 클라이언트가 구독할 수 있는 목적지 접두사
         registry.enableSimpleBroker("/topic");
-        // 클라이언트가 메시지를 보낼 때 사용하는 접두사
         registry.setApplicationDestinationPrefixes("/app");
     }
 }
