cc43a2f12ffbd54f1d7a8bf55729c22191004276
Hoooon22 <momo990305@gmail.com>
2025-03-12 23:23:06 +0900

    WebSocketConfig에서 SimpMessagingTemplate 빈 등록 코드 제거

--- src/main/java/com/hoooon22/devzip/Config/WebSocketConfig.java
+++ src/main/java/com/hoooon22/devzip/Config/WebSocketConfig.java
@@ -1,9 +1,6 @@
 package com.hoooon22.devzip.Config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -25,11 +22,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/topic");
         // 클라이언트가 메시지를 보낼 때 사용하는 접두사
         registry.setApplicationDestinationPrefixes("/app");
-    }
-    
-    // SimpMessagingTemplate 빈을 명시적으로 등록
-    @Bean
-    public SimpMessagingTemplate simpMessagingTemplate(MessageChannel clientOutboundChannel) {
-        return new SimpMessagingTemplate(clientOutboundChannel);
     }
 }
