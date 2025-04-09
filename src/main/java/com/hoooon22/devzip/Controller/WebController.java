14d1ae279298fa718b6fb06ac75b5b5fc9735fa1
Hoooon22 <momo990305@gmail.com>
2025-04-09 19:51:23 +0900

    WebController, application.properties에서 SPA 라우팅 설정 수정

--- src/main/java/com/hoooon22/devzip/Controller/WebController.java
+++ src/main/java/com/hoooon22/devzip/Controller/WebController.java
@@ -6,8 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
     
-    // Frontend 프로젝트의 라우팅 처리
-    @GetMapping(value = {"/", "/Guestbook", "/Joke", "/lolPatch", "/apiPage", "/dashboard", "/trendchat", "/error"})
+    // Frontend 프로젝트의 라우팅 처리 - 모든 경로를 인덱스 페이지로 포워드
+    @GetMapping(value = {
+        "/", 
+        "/Guestbook", 
+        "/Joke", 
+        "/lolPatch", 
+        "/apiPage", 
+        "/dashboard", 
+        "/trendchat", 
+        "/traceboard/**", 
+        "/chat/**",
+        "/error"
+    })
     public String index() {
         return "forward:/index.html";
     }
