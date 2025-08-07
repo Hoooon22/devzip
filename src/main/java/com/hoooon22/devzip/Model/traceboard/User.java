1758011c8bfcf536bd47e9820a49fc7228c7639c
Hoooon22 <momo990305@gmail.com>
2025-08-07 16:46:52 +0900

    Fix Entity name conflict between User and TraceBoardUser
    - Add @Entity(name = "TraceBoardUser") to traceboard User entity
    - Prevent Hibernate DuplicateMappingException
    - Ensure distinct entity names for JWT auth and traceboard systems
    - Allow proper JPA EntityManagerFactory initialization

--- src/main/java/com/hoooon22/devzip/Model/traceboard/User.java
+++ src/main/java/com/hoooon22/devzip/Model/traceboard/User.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-@Entity
+@Entity(name = "TraceBoardUser")
 @Table(name = "traceboard_users")
 @Data
 @EqualsAndHashCode(callSuper = true)
